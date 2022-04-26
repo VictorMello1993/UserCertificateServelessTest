d58ed1d5f7eb6b79afd6ff000984162c5da72866
Victor Mello <victor.mello@al.infnet.edu.br>
2022-04-25 23:18:24 -0300

    Conhecendo DynamoDB

--- src/utils/dynamodbClient.ts
+++ src/utils/dynamodbClient.ts
@@ -3,6 +3,8 @@ import {DynamoDB} from 'aws-sdk'
 const options = {
   region: "localhost",
   endpoint: "http://localhost:8000",
+  
+  //Sem credenciais
   accessKeyId: "x",
   secretAccessKey: "x"
 }
